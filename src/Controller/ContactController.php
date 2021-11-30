8c7fdeac9ecca39f32a00732712d24b4e69f0386
Aurelien THIERRY <aurelien.thierry@live.fr>
2021-11-30 18:22:23 +0100

    Ajout de style et page 404

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Mail;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/nous-contactez", name="contact")
+     */
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('notice', 'Merci de nous avoir contacté. Notre équipe reviens vers vous dans les meilleurs délais.');
+
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
